fdd5f372b0833fa62a674d6de768ebe676ad2c33
Roy Qian <seandsky@gmail.com>
2021-06-09 15:41:47 +0800

    Initial commit

---
+++ src/views/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="index">
+    <viewer @on-viewer-completed="viewerCompletedHandler"></viewer>
+  </div>
+</template>
+
+<script>
+import ViewerApi from '@/api/ViewerApi'
+export default {
+  name: 'Index',
+  components: {},
+  methods: {
+    viewerCompletedHandler(viewer) {
+      global.viewerApi = new ViewerApi(viewer)
+      global.viewerApi.addBaseLayer()
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.index {
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+}
+</style>
